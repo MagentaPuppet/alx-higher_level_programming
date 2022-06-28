15d140bb322963cf007df0598b3c38ce892ea80a
MagentaPuppet <paul.gabriel@bison.howard.edu>
2022-06-28 14:27:26 +0100

    added task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -4,6 +4,20 @@
 
 class Square:
     """Class that defines a square"""
-    def __init__(self, __size):
+    def __init__(self, size):
         """Init method for the square class"""
-        self.__size = __size
+        self.__size = size
+
+my_square = Square(3)
+print(type(my_square))
+print(my_square.__dict__)
+
+try:
+    print(my_square.size)
+except Exception as e:
+    print(e)
+
+try:
+    print(my_square.__size)
+except Exception as e:
+    print(e)
---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module for creating a class for a square"""
+
+
+class Square:
+    """Class that defines a square"""
+    def __init__(self, size=0):
+        """Init method for the square class"""
+        self.__size = __size
