bdd8b85dbe97610c0925c6e5599cc91d43208a19
MagentaPuppet <paul.gabriel@bison.howard.edu>
2022-06-28 15:29:53 +0100

    made 5-no_c.py executable and added task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,9 @@
-#!/usr/bin/bash
+#!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for i in matrix:
         for j in i:
-            print("{:d}".format(j), end=" ")
+            if j != i[len(i) - 1]:
+                print("{:d}".format(j), end=" ")
+            else:
+                print("{:d}".format(j), end="")
         print()
-    return
