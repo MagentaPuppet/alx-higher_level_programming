4cacc46ea38dde37564fbc9b3db844506b66370a
MagentaPuppet <paul.gabriel@bison.howard.edu>
2022-06-28 04:38:01 +0100

    added task 0

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-"""Module for creating a class for a square
+"""Module for creating a class for a square"""
 
-"""
+
 class Square:
-    """Empty class that defines a square
-
-    """
+    """Empty class that defines a square"""
     pass
