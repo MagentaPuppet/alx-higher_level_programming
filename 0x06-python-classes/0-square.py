1cd575c7b4fd119b570bda07bb81a68e9c581948
MagentaPuppet <paul.gabriel@bison.howard.edu>
2022-06-28 03:36:29 +0100

    added task 0

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -2,9 +2,4 @@
 """Module for creating a class for a square"""
 class Square:
     """Empty class that defines a square"""
-    def __init__(self):
-        """Init method for the square class"""
-        pass
-
-if __name__ == "__main__":
     pass
