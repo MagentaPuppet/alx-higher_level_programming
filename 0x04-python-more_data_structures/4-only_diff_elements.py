e5979c13da582c14cc35d23686a4f29d2bfc7f0d
MagentaPuppet <paul.gabriel@bison.howard.edu>
2022-06-29 17:55:03 +0100

    added task 4

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -5,4 +5,4 @@ def only_diff_elements(set_1, set_2):
     for i in range(len(joint)):
         if joint[i] not in (joint[(i + 1):] + joint[:i]):
             diff += [joint[i]]
-    return diff
+    return set(diff)
