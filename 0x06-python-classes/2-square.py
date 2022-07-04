9c99e65b08470af521c85f09034b42f658fa2ce7
MagentaPuppet <paul.gabriel@bison.howard.edu>
2022-07-04 18:25:23 +0100

    added task 2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -4,6 +4,10 @@
 
 class Square:
     """Class that defines a square"""
-    def __init__(self, size):
+    def __init__(self, size=0):
         """Init method for the square class"""
+        if not type(size) == int:
+            raise TypeError("size must be an integer")
+        if not size >= 0:
+            raise ValueError("size must be >= 0")
         self.__size = size
