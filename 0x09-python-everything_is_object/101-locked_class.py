ceb5a25d7597e186ca8a4e8ecf42411fddec2995
MagentaPuppet <paul.gabriel@bison.howard.edu>
2022-07-12 23:28:57 +0100

    added tasks

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""Module for creating a locked class"""
+
+
 class LockedClass:
+    """Class that only allows attributes with the name first_name"""
     __slots__ = ['first_name']
 
     def __init__(self):
