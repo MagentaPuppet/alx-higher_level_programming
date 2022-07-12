ded6443bc82b3ff45e715555d74c072862182658
MagentaPuppet <paul.gabriel@bison.howard.edu>
2022-07-12 23:24:44 +0100

    added tasks

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -2,12 +2,5 @@
 class LockedClass:
     __slots__ = ['first_name']
 
-    def __init__(self, first_name=''):
-        self.first_name = first_name
-
-lc = LockedClass()
-lc.first_name = "John"
-try:
-    lc.last_name = "Snow"
-except Exception as e:
-    print("[{}] {}".format(e.__class__.__name__, e))
+    def __init__(self):
+        pass
