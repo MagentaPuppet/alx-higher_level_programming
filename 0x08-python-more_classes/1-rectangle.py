b77ab3e9e00a929d9c1438e9bf80aee9d2bf131c
Paul Gabriel <paul.gabriel@bison.howard.edu>
2022-08-30 20:39:26 +0100

    added task 1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -6,8 +6,8 @@ class Rectangle:
     """class that defines a rectangle"""
     def __init__(self, width=0, height=0):
         """init method for the class Rectangle"""
-        self.height = height
-        self.width = width
+        self.__height = height
+        self.__width = width
 
     @property
     def width(self):
