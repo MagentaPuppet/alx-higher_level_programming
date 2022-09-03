617da781728ef1bea0379ce45a8545ef6d352947
Paul Gabriel <paul.gabriel@bison.howard.edu>
2022-09-03 20:08:33 +0100

    added advanced tasks 17 and 18

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -5,4 +5,4 @@ USE hbtn_0c_0;
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 -- converts the field name in first_table to UTF8
 USE hbtn_0c_0;
-ALTER TABLE first_table MODIFY `name` VARCHAR(256) CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table MODIFY `name` VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
