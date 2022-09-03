348d942295ab569886196ad316f6224eb6a5e88f
Paul Gabriel <paul.gabriel@bison.howard.edu>
2022-09-03 19:48:03 +0100

    added advanced tasks 17 and 18

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,2 +1,2 @@
 -- displays the average temperatures (Fahrenheit) by city ordered by temperature (decending)
-SELECT `city`, AVG(`value`) 'avg_temp' FROM `temperatures` GROUP BY `city` ORDER BY `value` DESC
+SELECT `city`, AVG(`value`) 'avg_temp' FROM `temperatures` GROUP BY `city` ORDER BY `avg_temp` DESC
