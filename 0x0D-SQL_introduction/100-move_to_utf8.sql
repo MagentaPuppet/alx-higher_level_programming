cd93fb40391f18ade6c5cf98e75f21d6af00d2c2
Paul Gabriel <paul.gabriel@bison.howard.edu>
2022-09-03 17:05:46 +0100

    added advanced tasks 17 and 18

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,5 +1,5 @@
 -- converts hbtn_0c_0 database to UTF8(utf8mb4, collate utf8mb4_unicode_ci)
-ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER DATABASE hbtn_0c_0 COLLATE utf8mb4_unicode_ci;
 -- converts table first_table to UTF8
 ALTER TABLE hbtn_0c_0.first_table CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 -- converts the field name in first_table to UTF8
